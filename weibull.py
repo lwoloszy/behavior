788f18d533809b5f75c346f9e75c4c9740c6b717
lwoloszy <luke.woloszyn@gmail.com>
2015-12-14 08:35:26 -0500

    fix weibull plots

--- weibull.py
+++ weibull.py
@@ -1,11 +1,129 @@
 from __future__ import division
 import numpy as np
+import seaborn as sns
 from scipy.optimize import fmin
 import matplotlib.pyplot as plt
-from matplotlib.ticker import NullFormatter
 from scipy.special import gammaln
 
 eps = 2 ** -52
+
+sns.set_style('ticks')
+sns.set_context('talk')
+
+
+def plotDataBernoulli(y, stim_strength, theta_init=[.1, 1]):
+    """plotDataBernoulli(y, stim_strength, theta_init = [.1,1])
+
+    Fit data to a Weibull function using a Bernoulli LL formulation and plot it
+    ----------------------------------------------------------------------------
+    y:
+      array-like (1d) containing a sequence of 0s (wrong) and 1s (correct)
+
+    stim_strength:
+      array-like (1d) containing a sequence of stim_strengtherences
+      corresponding to the y
+
+    theta_init:
+      list length 2 with initial estimates for alpha and beta
+
+    """
+
+    theta = fitDataBernoulli(y, stim_strength, theta_init)
+
+    counter = 0
+    ustim_strength = np.sort(np.unique(stim_strength))
+    ustim_strength = ustim_strength[ustim_strength > 0]
+    prop_correct = np.zeros(len(ustim_strength))
+    for i in ustim_strength:
+        prop_correct[counter] = np.mean(y[stim_strength == i])
+        counter += 1
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.scatter(
+        ustim_strength, prop_correct, marker='o', s=50, color='k')
+
+    xs = np.linspace(np.min(ustim_strength) * .8,
+                     np.max(ustim_strength) * 1.2, 100)
+    pred_y = calculateWeibullP(xs, theta[0], theta[1])
+    ax.add_line(plt.Line2D(xs, pred_y, color='black'))
+
+    ax.axvline(theta[0], color='black', linestyle='--', alpha=0.5)
+    ax.axhline(.816, color='black', linestyle='--', alpha=0.5)
+
+    ax.text(.1, .975, r'$\alpha$ = %.2f' %
+            theta[0], transform=ax.transAxes)
+    ax.text(.1, .925, r'$\beta$ = %.2f' %
+            theta[1], transform=ax.transAxes)
+
+    ax.set_xlim(np.min(ustim_strength) * .5, np.max(ustim_strength) * 1.1)
+    ax.set_xlim(.01, 1)
+    ax.set_ylim((.45, 1.05))
+    ax.set_xticks([10**np.arange(-3, 0)])
+    ax.set_xlabel('Stim strength')
+    ax.set_ylabel('Proportion correct')
+
+    # trim won't won't with x log axis so just leave it
+    sns.despine(offset=5)
+
+    ax.set_xscale('log')
+
+    plt.tight_layout()
+
+
+def plotDataBinomial(data):
+    """plotDataBinomial(data, theta_init = [.1,1])
+
+    Fit data to a Weibull function using a Binomial LL formulation and plot it
+    ---------------------------------------------------------------------------
+    data:
+      2d array, with n rows and 4 columns
+
+      1st column = stim_strengths
+      2nd column = proportion correct
+      3rd column = number of successes
+      4th column = total number of trials
+
+    theta_init:
+      list length 2 with initial estimates for alpha and beta
+
+    """
+
+    theta = fitDataBinomial(data)
+
+    ustim_strength = data[:, 0]
+    prop_correct = data[:, 1]
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.scatter(
+        ustim_strength, prop_correct, marker='o', s=50, color='k')
+
+    xs = np.linspace(np.min(ustim_strength) * .8,
+                     np.max(ustim_strength) * 1.2, 100)
+    pred_y = calculateWeibullP(xs, theta[0], theta[1])
+    ax.add_line(plt.Line2D(xs, pred_y, color='black'))
+
+    ax.axvline(theta[0], color='black', linestyle='--', alpha=0.5)
+    ax.axhline(.816, color='black', linestyle='--', alpha=0.5)
+
+    ax.text(.1, .975, r'$\alpha$ = %.2f' %
+            theta[0], transform=ax.transAxes)
+    ax.text(.1, .925, r'$\beta$ = %.2f' %
+            theta[1], transform=ax.transAxes)
+
+    ax.set_xlim(np.min(ustim_strength) * .5, np.max(ustim_strength) * 1.1)
+    ax.set_xlim(.01, 1)
+    ax.set_ylim((.45, 1.05))
+    ax.set_xticks([10**np.arange(-3, 0)])
+    ax.set_xlabel('Stim strength')
+    ax.set_ylabel('Proportion correct')
+    sns.despine(offset=5, bottom=False)
+
+    # trim won't won't with x log axis so just leave it
+    sns.despine(offset=5)
+
+    plt.tight_layout()
 
 
 def fitDataBernoulli(y, stim_strength, theta_init=[.1, 1]):
@@ -116,165 +234,3 @@ def calculateWeibullNegLLBinomial(theta, data):
         n1_observed * np.log(p_pred + eps) +
         (n_total - n1_observed) * np.log(1 - p_pred + eps))
     return(-LL)
-
-
-def plotWeibullBernoulli(y, stim_strength, theta_init=[.1, 1]):
-    theta = fitDataBernoulli(y, stim_strength, theta_init)
-
-    counter = 0
-    stim_strengtherences = np.sort(np.unique(stim_strength))
-    propCorrect = np.zeros(len(stim_strengtherences))
-    for i in stim_strengtherences:
-        propCorrect[counter] = np.mean(y[stim_strength == i])
-        counter += 1
-
-    fig = plt.figure()
-    nullfmt = NullFormatter()
-
-    left, width = 0.15, 0.75
-    bottom, height = 0.1, 0.85
-    rect1 = [left, bottom, width, height]
-    rect2 = [left - 0.05, bottom, 0.04, height]
-
-    axMain = fig.add_axes(rect1)
-    axMain.scatter(
-        stim_strengtherences[0:], propCorrect[0:], marker='o', s=50, color='k')
-
-    xs = np.arange(0, 1, .001)
-    fittedYs = calculateWeibullP(xs, theta[0], theta[1])
-    axMain.add_line(plt.Line2D(xs, fittedYs, linewidth=2, color='black'))
-
-    axMain.axvline(theta[0], color='black', linestyle='--')
-    axMain.axhline(.816, color='black', linestyle='--')
-
-    axMain.set_xlim((0.02, 1))
-    axMain.set_ylim((.35, 1.05))
-    axMain.set_title('Performance as a Function of Unsigned Stim_Strengtherence')
-    axMain.set_xlabel('Unsigned Stim_Strengtherence (%)')
-    axMain.set_xscale('log')
-    axMain.yaxis.set_major_formatter(nullfmt)
-
-    axMain.text(.1, .95, 'alpha = %.2f' %
-                theta[0], transform=axMain.transAxes)
-    axMain.text(.1, .925, 'beta = %.2f' %
-                theta[1], transform=axMain.transAxes)
-
-    # For zero percent stim_strengtherence (can't be on log axis)
-    axSide = fig.add_axes(rect2)
-    axSide.scatter(0, 0.5, marker='o', s=50, color='black')
-
-    xs = np.arange(0, 1, .001)
-    fittedYs = calculateWeibullP(xs, theta[0], theta[1])
-    axSide.add_line(plt.Line2D(xs, fittedYs, linewidth=2, color='black'))
-
-    axSide.set_ylabel('Proportion Correct')
-    axSide.set_xlim((-.0001, .001))
-    axSide.set_ylim((.35, 1.05))
-    axSide.xaxis.set_major_formatter(nullfmt)
-
-
-def plotWeibullBernoulliWithLapse(y, stim_strength, theta_init=[.1, 1, .05]):
-    theta = fitDataBernoulliWithLapse(y, stim_strength, theta_init)
-    print theta
-
-    counter = 0
-    stim_strengtherences = np.sort(np.unique(stim_strength))
-    propCorrect = np.zeros(len(stim_strengtherences))
-    for i in stim_strengtherences:
-        propCorrect[counter] = np.mean(y[stim_strength == i])
-        counter += 1
-
-    fig = plt.figure()
-    nullfmt = NullFormatter()
-
-    left, width = 0.15, 0.75
-    bottom, height = 0.1, 0.85
-    rect1 = [left, bottom, width, height]
-    rect2 = [left - 0.05, bottom, 0.04, height]
-
-    axMain = fig.add_axes(rect1)
-    axMain.scatter(
-        stim_strengtherences[0:], propCorrect[0:], marker='o', s=50, color='k')
-
-    xs = np.arange(0, 1, .001)
-    fittedYs = calculateWeibullPLapse(
-        xs, theta[0], theta[1], theta[2])
-    axMain.add_line(plt.Line2D(xs, fittedYs, linewidth=2, color='black'))
-
-    axMain.axvline(theta[0], color='black', linestyle='--')
-    axMain.axhline(.816, color='black', linestyle='--')
-
-    axMain.set_xlim((0.02, 1))
-    axMain.set_ylim((.35, 1.05))
-    axMain.set_title('Performance as a Function of Unsigned Stim_Strengtherence')
-    axMain.set_xlabel('Unsigned Stim_Strengtherence (%)')
-    axMain.set_xscale('log')
-    axMain.yaxis.set_major_formatter(nullfmt)
-
-    axMain.text(.1, .95, 'alpha = %.2f' %
-                theta[0], transform=axMain.transAxes)
-    axMain.text(.1, .925, 'beta = %.2f' %
-                theta[1], transform=axMain.transAxes)
-    axMain.text(.1, .900, 'lambda = %.2f' %
-                theta[2], transform=axMain.transAxes)
-
-    # For zero percent stim_strengtherence (can't be on log axis)
-    axSide = fig.add_axes(rect2)
-    axSide.scatter(0, 0.5, marker='o', s=50, color='black')
-
-    xs = np.arange(0, 1, .001)
-    fittedYs = calculateWeibullP(xs, theta[0], theta[1])
-    axSide.add_line(plt.Line2D(xs, fittedYs, linewidth=2, color='black'))
-
-    axSide.set_ylabel('Proportion Correct')
-    axSide.set_xlim((-.0001, .001))
-    axSide.set_ylim((.35, 1.05))
-    axSide.xaxis.set_major_formatter(nullfmt)
-
-
-def plotWeibullBinomial(data):
-    theta = fitDataBinomial(data)
-
-    stim_strengtherences = data[:, 0]
-    propCorrect = data[:, 1]
-
-    fig = plt.figure()
-    nullfmt = NullFormatter()
-
-    left, width = 0.15, 0.75
-    bottom, height = 0.1, 0.85
-    rect1 = [left, bottom, width, height]
-    rect2 = [left - 0.05, bottom, 0.04, height]
-
-    axMain = fig.add_axes(rect1)
-    axMain.scatter(stim_strengtherences, propCorrect, marker='o', s=50)
-
-    xs = np.arange(.01, 1, .001)
-    fittedYs = calculateWeibullP(xs, theta[0], theta[1])
-    axMain.add_line(plt.Line2D(xs, fittedYs, linewidth=2))
-
-    axMain.axvline(theta[0], color='black', linestyle='--')
-    axMain.axhline(.816, color='black', linestyle='--')
-
-    axMain.set_xlim((.1, 1))
-    axMain.set_ylim((.35, 1.05))
-    axMain.set_title('Performance as a Function of Unsigned Stim_Strengtherence')
-    axMain.set_xlabel('Unsigned Stim_Strengtherence (%)')
-    axMain.set_xscale('log')
-    axMain.yaxis.set_major_formatter(nullfmt)
-
-    axMain.text(1, 1, 'alpha = %.2f' % theta[0])
-    axMain.text(1, .975, 'beta = %.2f' % theta[1])
-
-    # For zero percent stim_strengtherence (can't be on log axis)
-    axSide = fig.add_axes(rect2)
-    axSide.scatter(0, .5, marker='o', s=50)
-
-    xs = np.arange(0, .001, .0001)
-    fittedYs = calculateWeibullP(xs, theta[0], theta[1])
-    axSide.add_line(plt.Line2D(xs, fittedYs, linewidth=2))
-
-    axSide.set_ylabel('Proportion Correct')
-    axSide.set_xlim((-.0001, .001))
-    axSide.set_ylim((.35, 1.05))
-    axSide.xaxis.set_major_formatter(nullfmt)
